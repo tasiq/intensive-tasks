d0d66e8bdb98c5ee949deba8032b002bf46e1190
user <tasiqq@gmail.com>
2023-11-25 18:46:08 +0300

    Chapter#1, Task#1; - добавлены тесты.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,20 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        for (int i = 0; i < 2000; i++) {
+            System.out.println(getAgeString(i));
+        }
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (age % 10 == 1 && age % 100 != 11) {
+            return "Вам" + " " + age + " " + "год";
+        }
+        if ((age % 10 == 2 && age % 100 != 12) || (age % 10 == 3 && age % 100 != 13) || (age % 10 == 4 && age % 100 != 14)) {
+            return "Вам" + " " + age + " " + "года";
+        }
+        return "Вам" + " " + age + " " + "лет";
     }
 }
